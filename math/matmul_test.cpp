ae5c7d1c4d3baf2978512c1980875a634365af9f
jeremyiwk <jeremy@micromelody.net>
2023-01-24 22:48:27 -0800

    Added matmul to complete matmul_2x2

--- math/matmul_test.cpp
+++ math/matmul_test.cpp
@@ -7,18 +7,17 @@
 using Eigen::MatrixXd;
 using namespace std;
 
+bool isPowerOfTwo(int n)
+{
+    return (ceil(log2(n)) == floor(log2(n)));
+}
+
 int poweroftwo(int N)
 {
     int D = (int)log2(N-1) + 1;
     long int p = pow(2,D);
     return p;
 }
-
-bool isPowerOfTwo(int n)
-{
-    return (ceil(log2(n)) == floor(log2(n)));
-}
-
 
 MatrixXd pad_to_square(MatrixXd A, int D)
 {
@@ -53,33 +52,38 @@ MatrixXd matmul_2x2(MatrixXd A, MatrixXd B)
     else
     {
         C.block(0,0,h,h) = matmul_2x2(A.block(0,0,h,h), B.block(0,0,h,h)) + matmul_2x2(A.block(0,h,h,h), B.block(h,0,h,h));
-    
         C.block(0,h,h,h) = matmul_2x2(A.block(0,0,h,h), B.block(0,h,h,h)) + matmul_2x2(A.block(0,h,h,h), B.block(h,h,h,h));
-    
         C.block(h,0,h,h) = matmul_2x2(A.block(h,0,h,h), B.block(0,0,h,h)) + matmul_2x2(A.block(h,h,h,h), B.block(h,0,h,h));
-    
         C.block(h,h,h,h) = matmul_2x2(A.block(h,0,h,h), B.block(0,h,h,h)) + matmul_2x2(A.block(h,h,h,h), B.block(h,h,h,h));
-        
         return C;
     }
 }
 
 
-// MatrixXd matmul(MatrixXd A, MatrixXd B)
-// {
-//     int N = A.rows();
-//     int K = A.cols();
-//     int L = B.rows();
-//     int M = B.cols();
+MatrixXd matmul(MatrixXd A, MatrixXd B)
+{
+    int N = A.rows();
+    int K = A.cols();
+    int L = B.rows();
+    int M = B.cols();
     
-//     assert(L == K);
+    assert(L == K);
     
-//     int Adim = max(N,L);
-//     int Bdim = max(L,M);
-//     int maxDim = max(Adim, Bdim);
-//     D = poweroftwo(maxDim);
+    int Adim = max(N,L);
+    int Bdim = max(L,M);
+    int maxDim = max(Adim, Bdim);
+    int D = poweroftwo(maxDim);
     
-// }
+    MatrixXd A0 = pad_to_square(A,D);
+    MatrixXd B0 = pad_to_square(B,D);
+    MatrixXd C0 = MatrixXd::Zero(D,D);
+    
+    C0 = matmul_2x2(A0,B0);
+    
+    MatrixXd C(N,M);
+    C = C0.block(0,0,N,M);
+    return C;
+}
 
 
 int main(void)
@@ -91,20 +95,25 @@ int main(void)
     printf("The smallest power of 2 greater than %d is %d\n", N, p);
     
     MatrixXd A(2,2);
-    A(0,0) = 3;
-    A(1,0) = 2.5;
+    A(0,0) = 1;
+    A(1,0) = 10;
     A(0,1) = -1;
     A(1,1) = A(1,0) + A(0,1);
     std::cout << A << std::endl;
     
-    MatrixXd C;
+    MatrixXd C(2,3);
     
-    C = 2*A;
+    C(0,0) = 1.0;
+    C(0,1) = 1.0;
+    C(0,2) = 1.0;
+    C(1,0) = 1.0;
+    C(1,1) = 1.0;
+    C(1,2) = 1.0;
     std::cout << C << std::endl;
     
     MatrixXd D;
     
-    D = matmul_2x2(A, C);
+    D = matmul(A, C);
     
     std::cout << D << std::endl;
 
